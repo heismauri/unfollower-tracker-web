e5d87929a5f8c97c22ff3f28ab5a4c0768c19b1a
Mauricio Villa <vbamauricio@gmail.com>
2023-01-11 10:42:54 -0300

    Added file validator and reorganized functions

--- main.js
+++ main.js
@@ -2,30 +2,58 @@ const formJSON = document.getElementById('json-form');
 const nonFollowersArea = document.getElementById('non-followers');
 
 const usersPrinter = (userList) => {
+  const htmlText = document.createElement('h4');
+  htmlText.innerHTML = `<strong>${userList.length}</strong> people are not following you back on <i>Instagram</i>`;
+  nonFollowersArea.insertAdjacentElement('beforeend', htmlText);
+  nonFollowersArea.classList.add('border-top', 'mt-3', 'pt-3');
   userList.forEach((user) => {
-    const htmlText = document.createElement('li');
-    htmlText.innerHTML = `${user} (<a href="https://instagram.com/${user}">https://instagram.com/${user}</a>)`
-    nonFollowersArea.insertAdjacentElement('beforeend', htmlText);
+    const userItem = document.createElement('li');
+    userItem.innerHTML = `${user} (<a href="https://instagram.com/${user}">https://instagram.com/${user}</a>)`;
+    nonFollowersArea.insertAdjacentElement('beforeend', userItem);
   });
-}
+};
 
 const parseJSONFile = async (file) => {
-  return new Promise((resolve, reject) => {
-    const fileReader = new FileReader()
-    fileReader.onload = event => resolve(JSON.parse(event.target.result))
-    fileReader.onerror = error => reject(error)
-    fileReader.readAsText(file)
+  return new Promise((resolve, _) => {
+    const fileReader = new FileReader();
+    fileReader.onload = (event) => resolve(JSON.parse(event.target.result));
+    fileReader.readAsText(file);
   });
+};
+
+const userListBuilder = async (file) => {
+  const json = await parseJSONFile(file);
+  const list = json[Object.keys(json)[0]];
+  return list.map((user) => user.string_list_data[0].value);
+};
+
+const fileValidator = (followingFile, followersFile) => {
+  if (!followingFile) {
+    return [false, 'Following input should include a file'];
+  }
+  if (!(followingFile.name === 'following.json')) {
+    return [false, `-${followingFile.name}- should be "following.json"`];
+  }
+  if (!followersFile) {
+    return [false, 'Followers input should include a file'];
+  }
+  if (!(followersFile.name === 'followers.json')) {
+    return [false, `-${followersFile.name}- should be "followers.json"`];
+  }
+  return [true, 'File is valid'];
 };
 
 formJSON.addEventListener('submit', async (event) => {
   event.preventDefault();
   const followingFile = event.currentTarget.followingfile.files[0];
   const followersFile = event.currentTarget.followersfile.files[0];
-  const followingJSON = await parseJSONFile(followingFile);
-  const followersJSON = await parseJSONFile(followersFile);
-  const following = followingJSON.relationships_following.map((user) => user.string_list_data[0].value)
-  const followers = followersJSON.relationships_followers.map((user) => user.string_list_data[0].value)
-  const nonFollowersList = following.filter(user => !followers.includes(user));
+  const [isFileValid, error] = fileValidator(followingFile, followersFile);
+  if (!isFileValid) {
+    alert(error);
+    return;
+  }
+  const followingList = await userListBuilder(followingFile);
+  const followersList = await userListBuilder(followersFile);
+  const nonFollowersList = followingList.filter((user) => !followersList.includes(user));
   usersPrinter(nonFollowersList);
 });
